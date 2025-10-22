6b236fe160ad78ac82f39a712005eba31f675596
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-10-21 21:30:18 -0500

    door test completed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // On page load, open all tiles whose dates are before today
 
 // On click of a tile flip it if the date is today or earlier
+
+const flipTest = document.querySelectorAll('.door');
+
+flipTest.forEach(day => {
+	day.addEventListener('click', () => {
+		day.classList.add('opened');
+	});
+});

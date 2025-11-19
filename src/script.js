42e608af1917615c8a6a19be878f1d135084ee80
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-11-18 18:09:13 -0600

    Add countdown timer, set present dates to correct December dates.

--- src/script.js
+++ src/script.js
@@ -1,30 +1,57 @@
+let christmasDate = new Date("12/25/2025");
+christmasDate.setHours(0, 0, 0, 0);
+
+const countdown = setInterval(function() {
+	const now = new Date().getTime();
+	const remainingTime = christmasDate - now;
+
+	const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+	
+	const displayDays = String(days).padStart(2, '0');
+    const displayHours = String(hours).padStart(2, '0');
+    const displayMinutes = String(minutes).padStart(2, '0');
+    const displaySeconds = String(seconds).padStart(2, '0');
+
+	if(remainingTime < 0)
+	{
+		clearInterval(countdown);
+	}
+
+	let countdownElement = document.querySelector('.countdown-timer');
+	countdownElement.textContent = displayDays + ":" + displayHours + ":" + displayMinutes + ":" + displaySeconds;
+}, 1000);
+
+
 let currentDate = new Date();
 currentDate.setHours(0, 0, 0, 0);
-let date1 = new Date("11/1/2025");
-let date2 = new Date("11/2/2025");
-let date3 = new Date("11/3/2025");
-let date4 = new Date("11/4/2025");
-let date5 = new Date("11/5/2025");
-let date6 = new Date("11/6/2025");
-let date7 = new Date("11/7/2025");
-let date8 = new Date("11/8/2025");
-let date9 = new Date("11/9/2025");
-let date10 = new Date("11/10/2025");
-let date11 = new Date("11/11/2025");
-let date12 = new Date("11/12/2025");
-let date13 = new Date("11/13/2025");
-let date14 = new Date("11/14/2025");
-let date15 = new Date("11/15/2025");
-let date16 = new Date("11/16/2025");
-let date17 = new Date("11/17/2025");
-let date18 = new Date("11/18/2025");
-let date19 = new Date("11/19/2025");
-let date20 = new Date("11/20/2025");
-let date21 = new Date("11/21/2025");
-let date22 = new Date("11/22/2025");
-let date23 = new Date("11/23/2025");
-let date24 = new Date("11/24/2025");
-let date25 = new Date("11/25/2025");
+let date1 = new Date("12/1/2025");
+let date2 = new Date("12/2/2025");
+let date3 = new Date("12/3/2025");
+let date4 = new Date("12/4/2025");
+let date5 = new Date("12/5/2025");
+let date6 = new Date("12/6/2025");
+let date7 = new Date("12/7/2025");
+let date8 = new Date("12/8/2025");
+let date9 = new Date("12/9/2025");
+let date10 = new Date("12/10/2025");
+let date11 = new Date("12/11/2025");
+let date12 = new Date("12/12/2025");
+let date13 = new Date("12/13/2025");
+let date14 = new Date("12/14/2025");
+let date15 = new Date("12/15/2025");
+let date16 = new Date("12/16/2025");
+let date17 = new Date("12/17/2025");
+let date18 = new Date("12/18/2025");
+let date19 = new Date("12/19/2025");
+let date20 = new Date("12/20/2025");
+let date21 = new Date("12/21/2025");
+let date22 = new Date("12/22/2025");
+let date23 = new Date("12/23/2025");
+let date24 = new Date("12/24/2025");
+let date25 = new Date("12/25/2025");
 
 // Dictionary of "date" => "present id"
 const dateIdMap = {
